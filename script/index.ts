090a835932bc4d7996fbb7551e0817cee9dc97d2
Anatoli Avetisovi <tolikatolika794@gmail.com>
2022-08-21 18:12:20 +0400

    added  fetching functional

---
+++ script/index.ts
@@ -0,0 +1,21 @@
+interface Quote {
+  content: string;
+  author: string;
+}
+
+const quoteElement: HTMLElement | null = document.querySelector('.quote');
+const authorElement: HTMLElement | null = document.querySelector('.author');
+
+async function fetchQuote(): Promise<Quote> {
+  const request = await fetch('https://api.quotable.io/random');
+  const response = await request.json();
+  return response;
+}
+
+async function displayQuote(): Promise<void> {
+  const quote = await fetchQuote();
+  if (quoteElement) quoteElement.innerText = quote.content;
+  if (authorElement) authorElement.innerText = quote.author;
+}
+
+displayQuote();
